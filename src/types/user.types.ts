2b2f7c4f253bdf7fe76189dd3908da4d99a0916c
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2025-01-20 19:15:48 -0300

    fix user types. Remove ADB

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -50,7 +50,7 @@ export interface User {
 }
 
 export interface UsersResponse {
-  isSingleUser: number;
+  hasSingleMaestro: boolean;
   maestros: [];
   user: User;
   vistas: { id_vista: string; is_habilitado: boolean }[];
