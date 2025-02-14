f7b2c1e09e53186ea20882d879fe257d2727ea9e
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2025-02-14 12:51:37 -0300

    reorider style

--- src/app/repertoires/[id]/titularity/page.tsx
+++ src/app/repertoires/[id]/titularity/page.tsx
@@ -14,6 +14,7 @@ import { GetRepertoireTitularityResponse } from "@/types/repertoire.types";
 import { useEffect, useState } from "react";
 import { getRepertoireTitularity } from "@/services/repertoire";
 import { toast } from "react-toastify";
+import Spinner from "@/commons/Spinner/Spinner";
 
 function page() {
   const router = useRouter();
@@ -53,41 +54,50 @@ function page() {
     <CustomLayout>
       <Header back title="Titularidad" />
       <div className="w-[100%] flex-1 flex flex-col space-y-[1rem] overflow-y-auto">
-        <CustomTable
-          columnNames={[
-            { name: "PRODUCTORA", isSortable: true },
-            { name: "PORCENTAJE", isSortable: true },
-            { name: "REGISTRO DESDE", isSortable: true },
-            { name: "REGISTRO HASTA", isSortable: true },
-            { name: "ACCIÓN", isSortable: false },
-          ]}
-          columnValues={titularities.map((t) => [
-            t.productoraDeParticipante.nombre_productora,
-            t.porcentaje_participacion,
-            t.fecha_participacion_inicio,
-            t.fecha_participacion_hasta,
-            <ActionDropdownButton
-              menuOptions={[
-                {
-                  label: "Editar",
-                  icon: <MdEdit />,
-                  onClick: () =>
-                    redirectToOption(
-                      `/repertoires/${id}/titularity/edit-titular`
-                    ),
-                },
-                {
-                  label: "Editar",
-                  icon: <RiProhibited2Line />,
-                  onClick: () =>
-                    handleOpenModal(ModalNames.TITULARITY_PHOGRAM_REMOVE),
-                },
+        {loading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <Spinner color="black" />
+          </div>
+        ) : (
+          titularities &&
+          titularities.length > 0 && (
+            <CustomTable
+              columnNames={[
+                { name: "PRODUCTORA", isSortable: true },
+                { name: "PORCENTAJE", isSortable: true },
+                { name: "REGISTRO DESDE", isSortable: true },
+                { name: "REGISTRO HASTA", isSortable: true },
+                { name: "ACCIÓN", isSortable: false },
               ]}
-            />,
-          ])}
-        />
+              columnValues={titularities.map((t) => [
+                t.productoraDeParticipante.nombre_productora,
+                t.porcentaje_participacion,
+                t.fecha_participacion_inicio,
+                t.fecha_participacion_hasta,
+                <ActionDropdownButton
+                  menuOptions={[
+                    {
+                      label: "Editar",
+                      icon: <MdEdit />,
+                      onClick: () =>
+                        redirectToOption(
+                          `/repertoires/${id}/titularity/edit-titular`
+                        ),
+                    },
+                    {
+                      label: "Editar",
+                      icon: <RiProhibited2Line />,
+                      onClick: () =>
+                        handleOpenModal(ModalNames.TITULARITY_PHOGRAM_REMOVE),
+                    },
+                  ]}
+                />,
+              ])}
+            />
+          )
+        )}
       </div>
-      <div className="w-[100%] py-[1rem] flex items-center justify-end px-[2rem]">
+      <div className="w-[100%] py-[1rem] px-[2rem] flex items-center justify-end ">
         <CustomButton
           onClick={() =>
             redirectToOption(`/repertoires/${id}/titularity/add-titular`)
