a809c454ed926c53edfabc35153849c9a5769480
Tomas Suarez Lissi <tomas.sl@hotmail.com>
2025-02-19 20:26:05 -0300

    LLamo set active session

--- src/components/LoginView/LoginView.tsx
+++ src/components/LoginView/LoginView.tsx
@@ -10,7 +10,11 @@ import gitLogo from "../../assets/GIT LOGO.png";
 import CustomField from "@/commons/CustomField/CustomField";
 import CustomButton from "@/commons/CustomButton/CustomButton";
 import { validationLoginForm } from "@/utils/formValidations";
-import { authLogin, getAuthData } from "@/services/auth";
+import {
+  authLogin,
+  getAuthData,
+  selectProductionCompany,
+} from "@/services/auth";
 import { useAppDispatch, useAppSelector } from "@/hooks/storeHooks";
 import { setAuthData } from "@/store/authSlice";
 import { ROLES } from "@/types/auth.types";
@@ -47,7 +51,7 @@ const LoginForm: FC = () => {
     }
 
     const data = await getAuthData();
-    dispatch(setAuthData(data));
+
     if (data.estado === "HABILITADO") {
       router.push("/repertoires");
     } else {
@@ -60,6 +64,17 @@ const LoginForm: FC = () => {
     ) {
       openModal(<ChangeProducerModal />);
     }
+
+    if (data.productoras?.length === 1) {
+      await selectProductionCompany(data.productoras[0].id);
+      if (window && window.localStorage) {
+        localStorage.setItem("company", JSON.stringify(data.productoras[0]));
+      }
+    }
+
+    dispatch(
+      setAuthData({ ...data, productoraActiva: data.productoras?.[0] ?? null })
+    );
   };
 
   return (
